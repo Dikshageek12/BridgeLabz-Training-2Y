168d40dab5fd2fe5229db6ca23554dbc231efc72
Diksha <dikshasharma8795@gmail.com>
2025-08-14 11:25:31 +0530

    [Diksha] Average of Three NUmber.java

---
+++ AverageOfThreeNumber.java
@@ -0,0 +1,11 @@
+public class AverageOfThreeNumber {
+   public static void main(String[] args) {
+       double num1 = 10.5;
+       double num2 = 20.5;
+       double num3 = 30.5;
+
+       double average = (num1 + num2 + num3) / 3;
+
+       System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is: " + average);
+   }
+}
