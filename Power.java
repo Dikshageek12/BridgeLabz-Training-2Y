cf29c5add8e3d3dca968c25a2282b57b004d7177
Diksha <dikshasharma8795@gmail.com>
2025-08-14 11:22:00 +0530

    [Diksha] Power of number.java

---
+++ Power.java
@@ -0,0 +1,8 @@
+public class Power {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        double result = Math.pow(base, exponent);
+        System.out.println("The result of " + base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
